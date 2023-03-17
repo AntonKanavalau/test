2b4d19cc0fb03ac8b0a4c0165f5d40d624cf409d
Anton Kanavalau <old.projecta@gmail.com>
2023-03-17 20:30:03 +0300

    fix warning and error

--- script/validationForm.js
+++ script/validationForm.js
@@ -1,7 +1,6 @@
 'use strict'
 
 
-
 document.addEventListener('DOMContentLoaded', () => {
 	const form = document.forms['formSignUp'];
 	form.addEventListener('submit', formSend);
@@ -9,10 +8,17 @@ document.addEventListener('DOMContentLoaded', () => {
 	async function formSend(e) {
 		e.preventDefault();
 
-		let error = formValidate(form);
+		let error = formValidate();
+
+		if (error === 0) {
+
+		} else {
+			alert('Fill in all fields.');
+		}
+
 	}
 
-	function formValidate(form) {
+	function formValidate() {
 		let error = 0;
 
 		let formReq = document.querySelectorAll('._req');
@@ -21,30 +27,33 @@ document.addEventListener('DOMContentLoaded', () => {
 			const input = formReq[i];
 			formRemoveError(input);
 
-			switch (input.id)	{
+			switch (input.id) {
 				case 'firstName':
-					if(nameTest(input)) {
+					if (nameTest(input)) {
 						formAddError(input);
 						error++;
 					}
 					break;
 
 				case 'lastName':
-					if(nameTest(input)) {
+					if (nameTest(input)) {
 						formAddError(input);
 						error++;
 					}
 					break;
 
 				case 'eMail':
-					if(emailTest(input)){
+					if (emailTest(input)) {
 						formAddError(input);
+						emailAddError(input);
 						error++;
+					} else {
+						emailRemoveError(input);
 					}
 					break;
 
 				case 'password':
-					if(passwordTest(input)) {
+					if (passwordTest(input)) {
 						formAddError(input);
 						passAddError(input);
 						error++;
@@ -54,7 +63,7 @@ document.addEventListener('DOMContentLoaded', () => {
 					break;
 
 				case 'confirmPass':
-					if(confPasswordTest(input)) {
+					if (confPasswordTest(input)) {
 						formAddError(input);
 						confPassAddError(input);
 						error++;
@@ -62,36 +71,50 @@ document.addEventListener('DOMContentLoaded', () => {
 						confPassRemoveError(input);
 					}
 					break;
-
 			}
-
 		}
+		return error;
 	}
 
 	/*add redline*/
-	function formAddError(input){
+	function formAddError(input) {
 		input.parentElement.classList.add('redLine');
 	}
 
 	/*remove redline*/
-	function formRemoveError(input){
+	function formRemoveError(input) {
 		input.parentElement.classList.remove('redLine');
 	}
 
 	/*check first and last name*/
 	function nameTest(input) {
-		if(input.value.length === 0 || /[0-9]/.test(input.value) === true){
+		if (input.value.length === 0 || /[0-9]/.test(input.value) === true) {
 			return true;
 		}
 	}
 
 	/*check email*/
-	function emailTest(input){
+	function emailTest(input) {
 		return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value)
 	}
 
-	/*check password*/
-	function passAddError(input){
+	/*email error*/
+	function emailAddError(input) {
+		const html = `<span class="emailError">Incorrect e-Mail.</span>`;
+		if (input.parentElement.querySelector('.emailError')) {
+			return true;
+		} else {
+			input.parentElement.style.color = 'red';
+			input.parentElement.insertAdjacentHTML('beforeend', html);
+		}
+	}
+
+	function emailRemoveError(input) {
+		input.parentElement.querySelector('.emailError').remove();
+	}
+
+	/*conf error text*/
+	function passAddError(input) {
 		const html = `<span class="passError">Password must have uppercase letters, numbers and 8 symbols.</span>`;
 		if (input.parentElement.querySelector('.passError')) {
 			return true;
@@ -100,38 +123,38 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 	}
 
-	function passRemoveError(input){
-			input.parentElement.querySelector('.passError').remove();
+	function passRemoveError(input) {
+		input.parentElement.querySelector('.passError').remove();
 	}
 
 
 	/*check uppercase and number*/
-	function passwordTest(input){
-		if(upperCaseTest(input.value) || numberTest(input.value)){
+	function passwordTest(input) {
+		if (upperCaseTest(input.value) || numberTest(input.value) || input.value.length < 8) {
 			return true;
 		}
 	}
+
 	/*check uppercase*/
-	function upperCaseTest(value){
+	function upperCaseTest(value) {
 		return !/[A-ZА-Я]/gm.test(value);
 	}
 
 	/*check number*/
-	function numberTest(value){
+	function numberTest(value) {
 		return !/[0-9]/gm.test(value);
 	}
 
 
 	/*check confirm password*/
-	function confPasswordTest(input){
-		if(input.value !== input.password){
-			console.log(input);
+	function confPasswordTest(input) {
+		if (input.value !== input.password) {
 			return true;
 		}
 	}
 
-	/**/
-	function confPassAddError(input){
+	/*conf error text*/
+	function confPassAddError(input) {
 		const html = `<span class="conPassError">Passwords must match.</span>`;
 		if (input.parentElement.querySelector('.conPassError')) {
 			return true;
@@ -140,11 +163,10 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 	}
 
-	function  confPassRemoveError(input){
+	function confPassRemoveError(input) {
 		input.parentElement.querySelector('.conPassError').remove();
 	}
 })
-
 
 
 /*Uppercase first latter first and last name*/
